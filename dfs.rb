fb97b61908db0b01de595738a2015f3c5d212c36
Logan Donoughe <logandonoughe@gmail.com>
2020-04-22 13:48:55 -0700

    Fixes DFS

--- dfs.rb
+++ dfs.rb
@@ -2,13 +2,18 @@
 
 # Depth First Search
 def dfs(g, v, c)
-  require 'pry'; binding.pry if v.content == 'c'
   return v if v.content == c
   v.visited = true
   # for all directed edges from v to w that are in G.adjacent_edges(v) do
+  values = []
   v.edges.each do |w|
     unless w.visited
-      dfs(g, w, c)
+      values += [dfs(g, w, c)]
     end
+  end
+  if values&.empty?
+    return nil
+  else
+    return values.reject { |el| el == nil }[0]
   end
 end
